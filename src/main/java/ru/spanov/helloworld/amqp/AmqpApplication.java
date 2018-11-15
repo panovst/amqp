c351d549d3532aebf6d0579d64d2bc43a38da87a
spanov <test@test.org>
2018-11-15 21:19:00 +0300

    first example of amqp consummer

--- src/main/java/ru/spanov/helloworld/amqp/AmqpApplication.java
+++ src/main/java/ru/spanov/helloworld/amqp/AmqpApplication.java
@@ -2,11 +2,33 @@ package ru.spanov.helloworld.amqp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class AmqpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmqpApplication.class, args);
 	}
+
+    @StreamListener(Sink.INPUT)
+    public void handle(Person person) {
+        System.out.println("Received: " + person);
+    }
+
+    public static class Person {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String toString() {
+            return this.name;
+        }
+    }
 }
